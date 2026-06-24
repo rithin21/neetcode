92604af3c2fe5c28cd7aeff2192b4cd7aa089512
Rithin <rithinv21@gmail.com>
2026-06-24 10:04:33 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,77 @@
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    HashMap<Integer, Node> map;
+    Node head, tail;
+    int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void add(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+
+        Node node = map.get(key);
+
+        remove(node);
+        add(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+
+            node.value = value;
+
+            remove(node);
+            add(node);
+        } else {
+
+            if (map.size() == capacity) {
+                Node lru = tail.prev;
+
+                remove(lru);
+                map.remove(lru.key);
+            }
+
+            Node node = new Node(key, value);
+
+            add(node);
+            map.put(key, node);
+        }
+    }
+}
