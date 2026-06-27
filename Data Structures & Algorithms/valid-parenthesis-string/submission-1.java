f995a95253cdea3fd3cb6b75d58f8b9f68811adb
Rithin <rithinv21@gmail.com>
2026-06-27 15:02:11 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.java
@@ -0,0 +1,50 @@
+
+
+class Solution {
+    public boolean checkValidString(String s) {
+
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                open.push(i);
+            }
+            else if (ch == '*') {
+                star.push(i);
+            }
+            else { // ')'
+
+                if (!open.isEmpty()) {
+                    open.pop();
+                }
+                else if (!star.isEmpty()) {
+                    star.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        // Match remaining '(' with '*' that appear after them
+        while (!open.isEmpty()) {
+
+            if (star.isEmpty()) {
+                return false;
+            }
+
+            if (star.peek() < open.peek()) {
+                return false;
+            }
+
+            open.pop();
+            star.pop();
+        }
+
+        return true;
+    }
+}
