281fdf8d42905ab20d188b47b199cfa4a1472346
Rithin <rithinv21@gmail.com>
2026-07-02 19:16:59 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]+=1;
+                return digits;
+            }
+            else
+            {
+                digits[i]=0;
+            }
+        }
+        int[]ans=new int[digits.length+1];
+        ans[0]=1;
+        return ans;
+    }
+}
